9399b6260500d47656833fdc455a4ee9baf185f6
Brandon Rodak <brodak@gmail.com>
2013-07-11 17:45:46 +0000

    Added Express server line

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ // response.send('Hello World 2!');
+    res.render("index.html");
 });
 
 var port = process.env.PORT || 5000;
