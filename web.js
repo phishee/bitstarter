ae3746442a9e736b6755d83dae2ec0cb257ebad2
Ubuntu <ubuntu@ip-172-31-46-93.us-west-2.compute.internal>
2013-07-17 15:36:57 +0000

    Modified server port

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+var htmlfile = "index.html";
 
 app.get('/', function(request, response) {
      // response.send('Hello World 2!');
-     fs.readFile('index.html', 'utf8', function(err, text){
-     response.send(text);
-});
+     var html = fs.readFileSync(htmlfile).toString();
+     response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
